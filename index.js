8b9ba83259ca5c977b17cbed9363d4686e6eab87
apangasa <apangasa@adobe.com>
2021-04-19 00:10:43 +0530

    Node Callbacks and Error Handling

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var rect = require("./rectangle.js");
 function solveRect(x, y){
     console.log("Solving for x = " +x + " and y = " + y);
 
-    if(x <=0 || y <= 0){
-        console.log("x and y should be greater than 0");
-    }
-    else{
-        console.log("rectange area = " + rect.area + ", rectangle perimeter = " + rect.perimeter);
-    }
+   rect(x,y, (err, rect) => {
+       if(err){
+           console.log("Encountered " + err.message);
+       }
+       else{
+           console.log("Area is " + rect.area() + ", Perimeter is " + rect.perimeter());
+       }
+   });
+   console.log("This statement after the call to rect()");
 }
 
 solveRect(1,1);
